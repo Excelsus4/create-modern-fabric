3159224ceb80334de3936152d3bfc2820a1e1016
Excelsus4 <elijahko4@gmail.com>
2022-12-28 01:41:25 +0900

    Initial commit

--- src/main/java/com/excelsus/createmodern/CreateModern.java
+++ src/main/java/com/excelsus/createmodern/CreateModern.java
@@ -1,21 +1,30 @@
-package net.fabricmc.example;
+package com.excelsus.createmodern;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class CreateModern implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("Create:Modern");
+
+	// sample instance of the mod
+	public static final Item MOD_ITEM =
+		Registry.register(Registry.ITEM, new Identifier("create_modern", "stainless_steel_ingot"),
+			new Item(new FabricItemSettings()));
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
 	}
 }
