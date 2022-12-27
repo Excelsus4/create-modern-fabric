9bc1f32731c6216a43cb428c974666c677ee7de6
Excelsus4 <elijahko4@gmail.com>
2022-12-28 03:48:02 +0900

    First item ever added

--- src/main/java/com/excelsus/createmodern/CreateModern.java
+++ src/main/java/com/excelsus/createmodern/CreateModern.java
@@ -1,21 +1,30 @@
 package com.excelsus.createmodern;
 
+import com.mojang.logging.LogUtils;
 import com.simibubi.create.Create;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CreateModern implements ModInitializer {
-	public static final String ID = "CreateModern";
-	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+	public static final String ID = "create_modern";
+	public static final String NAME = "CreateModern";
+	public static final Logger LOGGER = LogUtils.getLogger();
+
+	// raw CHROME!
+	public static final Item RAW_CHROME =
+		Registry.register(Registry.ITEM, new ResourceLocation(ID, "raw_chrome"),
+			new Item(new FabricItemSettings()));
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Create addon mod [{}] loading alongside Create [{}]!", ID, Create.VERSION);
+		LOGGER.info("Create addon mod [{}] loading alongside Create [{}]!", NAME, Create.VERSION);
 	}
 
 	public static ResourceLocation id(String path) {
