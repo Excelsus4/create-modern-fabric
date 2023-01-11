630893466eb0449c4a1463db6a4a987887490f3d
Excelsus4 <elijahko4@gmail.com>
2023-01-12 02:12:48 +0900

    Item Registrate setup in Create way

--- src/main/java/com/excelsus/createmodern/AllItems.java
+++ src/main/java/com/excelsus/createmodern/AllItems.java
@@ -1,4 +1,32 @@
 package com.excelsus.createmodern;
 
+import static com.simibubi.create.AllTags.*;
+
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+import com.simibubi.create.content.AllSections;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.tterrag.registrate.util.entry.ItemEntry;
+
 public class AllItems {
+	private static final CreateRegistrate REGISTRATE = CreateModern.registrate()
+		.creativeModeTab(() -> CreateModern.MODERN_CREATIVE_TAB);
+
+	static {
+		REGISTRATE.startSection(AllSections.MATERIALS);
+	}
+
+	public static final ItemEntry<Item> RAW_CHROME =
+		taggedIngredient("raw_chrome", forgeItemTag("raw_materials/chrome"), forgeItemTag("raw_materials"));
+
+	@SafeVarargs
+	private static ItemEntry<Item> taggedIngredient(String name, TagKey<Item>... tags) {
+		return REGISTRATE.item(name, Item::new)
+			.tag(tags)
+			.register();
+	}
+
+	// Load this class
+	public static void register() {}
 }
--- src/main/java/com/excelsus/createmodern/CreateModern.java
+++ src/main/java/com/excelsus/createmodern/CreateModern.java
@@ -4,15 +4,14 @@ import static com.excelsus.createmodern.AllItems.*;
 
 import com.mojang.logging.LogUtils;
 import com.simibubi.create.Create;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.tterrag.registrate.util.nullness.NonNullSupplier;
 
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 import org.slf4j.Logger;
@@ -22,25 +21,25 @@ public class CreateModern implements ModInitializer {
 	public static final String NAME = "CreateModern";
 	public static final Logger LOGGER = LogUtils.getLogger();
 
-	// ==== TUTORIAL CODES ====
-	// raw CHROME!
-	public static final Item RAW_CHROME =
-		Registry.register(Registry.ITEM, new ResourceLocation(ID, "raw_chrome"),
-			new Item(new FabricItemSettings()));
-
-	public static CreativeModeTab TAB;
-	// ========
+	private static final NonNullSupplier<CreateRegistrate> REGISTRATE = CreateRegistrate.lazy(ID);
+	public static final CreativeModeTab MODERN_CREATIVE_TAB = FabricItemGroupBuilder.build(
+		new ResourceLocation(ID, "modern"),
+		() -> new ItemStack(RAW_CHROME.get())
+	);
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Create addon mod [{}] loading alongside Create [{}]!", NAME, Create.VERSION);
-		FabricItemGroupBuilder builder = FabricItemGroupBuilder.create(new ResourceLocation(ID, "tab"));
-		builder.appendItems((stacks) -> {
-			stacks.add(new ItemStack(RAW_CHROME));
-		});
-		builder.icon(() -> new ItemStack(RAW_CHROME));
-		TAB = builder.build();
+		onCtor();
+	}
 
+	public static void onCtor() {
+		AllItems.register();
+		REGISTRATE.get().register();
+	}
+
+	public static CreateRegistrate registrate() {
+		return REGISTRATE.get();
 	}
 
 	public static ResourceLocation id(String path) {
